e00c035cb30a54d34dae095421e7f55f6a52d40c
doctorseus <doctorseus@devlabs.pro>
2016-08-01 00:03:15 +0200

    Moved code into ArUcoNode class.

--- tuw_aruco/src/aruco_node.cpp
+++ tuw_aruco/src/aruco_node.cpp
@@ -1,15 +1,37 @@
-#include "ros/ros.h"
-#include "std_msgs/String.h"
-#include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
-#include <opencv2/highgui/highgui.hpp>
-#include "aruco.h"
+#include "aruco_node.hpp"
 
-static aruco::MarkerDetector detector;
-static std::map<uint32_t, aruco::MarkerPoseTracker> tracker;
-static aruco::CameraParameters camParams;
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "arMarker");
+    ros::NodeHandle n;
+    ArUcoNode arUcoNode(n);
+    ros::spin();
+    return 0;
+}
 
-void imageCallback(const sensor_msgs::ImageConstPtr &image_msg, const sensor_msgs::CameraInfoConstPtr &camer_info_) {
+ArUcoNode::ArUcoNode(ros::NodeHandle &n) : _n(n), _imageTransport(n) {
+    _detector.setDictionary(aruco::Dictionary::ARTOOLKITPLUSBCH);//sets the dictionary to be employed (ARUCO,APRILTAGS,ARTOOLKIT,etc)
+    _detector.setThresholdParams(7, 7);
+    _detector.setThresholdParamRange(2, 0);
+
+
+    // Camera parameters
+    float camera_matrix_data[9] = {844.196880, 0.000000, 318.514635, 0.000000, 852.683290, 254.889584, 0.000000, 0.000000, 1.000000};
+    cv::Mat camera_matrix = cv::Mat(3, 3, CV_32F, camera_matrix_data);
+
+    float distortion_coefficients_data[5] = {0.130185, -0.499155, -0.009280, -0.003387, 0.000000};
+    cv::Mat distortion_coefficients = cv::Mat(1, 5, CV_32F, distortion_coefficients_data);
+
+    _camParams = aruco::CameraParameters(camera_matrix, distortion_coefficients, cv::Size(640, 480));
+
+    // Subscribe to image topic
+    _cameraSubscriber = _imageTransport.subscribeCamera("image", 1, &ArUcoNode::imageCallback, this);
+
+    cv::namedWindow("aruco_node_debug");
+}
+
+ArUcoNode::~ArUcoNode() {}
+
+void ArUcoNode::imageCallback(const sensor_msgs::ImageConstPtr &image_msg, const sensor_msgs::CameraInfoConstPtr &camer_info_) {
     cv_bridge::CvImagePtr img;
     try {
         img = cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::MONO8);
@@ -17,10 +39,35 @@ void imageCallback(const sensor_msgs::ImageConstPtr &image_msg, const sensor_msg
 
         // Detect markers
         vector< aruco::Marker > markers;
-        markers = detector.detect(img->image);
+        markers = _detector.detect(img->image);
 
         for (auto &marker:markers)//for each marker
-            tracker[marker.id].estimatePose(marker, camParams, 6.0f); //call its tracker and estimate the pose
+            _tracker[marker.id].estimatePose(marker, _camParams, 6.0f); //call its tracker and estimate the pose
+
+
+        // Publish transforms
+        _markerTransforms.clear();
+
+        tf::StampedTransform st;
+        for (auto &marker:markers) {
+            /*
+            //returns the 4x4 transform matrix. Returns an empty matrix if last call to estimatePose returned false
+            cv::Mat getRTMatrix()const;
+
+            //return the rotation vector. Returns an empty matrix if last call to estimatePose returned false
+            const cv::Mat getRvec()const{return _rvec;}
+
+            //return the translation vector. Returns an empty matrix if last call to estimatePose returned false
+            const cv::Mat getTvec()const{return _tvec;}
+            */
+
+
+            //cv::Mat m = tracker[marker.id].getRTMatrix();
+
+
+            st = tf::StampedTransform();
+            _markerTransforms.push_back(st);
+        }
 
 
         // Draw markers
@@ -32,8 +79,8 @@ void imageCallback(const sensor_msgs::ImageConstPtr &image_msg, const sensor_msg
             markers[i].draw(debugImage, cv::Scalar(0, 0, 255), 1);
 
             // draw a 3d cube
-            aruco::CvDrawingUtils::draw3dCube(debugImage, markers[i], camParams);
-            aruco::CvDrawingUtils::draw3dAxis(debugImage, markers[i], camParams);
+            aruco::CvDrawingUtils::draw3dCube(debugImage, markers[i], _camParams);
+            aruco::CvDrawingUtils::draw3dAxis(debugImage, markers[i], _camParams);
         }
 
         cv::imshow("aruco_node_debug", debugImage);
@@ -44,32 +91,3 @@ void imageCallback(const sensor_msgs::ImageConstPtr &image_msg, const sensor_msg
     }
 }
 
-int main(int argc, char **argv) {
-    ros::init(argc, argv, "arMarker");
-
-    ros::NodeHandle n;
-
-
-    detector.setDictionary(aruco::Dictionary::ARTOOLKITPLUSBCH);//sets the dictionary to be employed (ARUCO,APRILTAGS,ARTOOLKIT,etc)
-    detector.setThresholdParams(7, 7);
-    detector.setThresholdParamRange(2, 0);
-
-
-    // Camera parameters
-    float camera_matrix_data[9] = {844.196880, 0.000000, 318.514635, 0.000000, 852.683290, 254.889584, 0.000000, 0.000000, 1.000000};
-    cv::Mat camera_matrix = cv::Mat(3, 3, CV_32F, camera_matrix_data);
-
-    float distortion_coefficients_data[5] = {0.130185, -0.499155, -0.009280, -0.003387, 0.000000};
-    cv::Mat distortion_coefficients = cv::Mat(1, 5, CV_32F, distortion_coefficients_data);
-
-    camParams = aruco::CameraParameters(camera_matrix, distortion_coefficients, cv::Size(640, 480));
-
-    cv::namedWindow("aruco_node_debug");
-
-    image_transport::ImageTransport imageTransport(n);
-    image_transport::CameraSubscriber cameraSubscriber = imageTransport.subscribeCamera("image", 1, &imageCallback);
-
-
-    ros::spin();
-    return 0;
-}
---
+++ tuw_aruco/src/aruco_node.hpp
@@ -0,0 +1,37 @@
+#ifndef ARUCO_NODE_H
+#define ARUCO_NODE_H
+
+#include "ros/ros.h"
+#include <image_transport/image_transport.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
+
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/highgui/highgui.hpp>
+
+#include "aruco.h"
+
+class ArUcoNode {
+public:
+    ArUcoNode(ros::NodeHandle &n);
+
+    ~ArUcoNode();
+
+private:
+    ros::NodeHandle _n;
+
+    image_transport::ImageTransport _imageTransport;
+    image_transport::CameraSubscriber _cameraSubscriber;
+
+    aruco::MarkerDetector _detector;
+    std::map<uint32_t, aruco::MarkerPoseTracker> _tracker;
+    aruco::CameraParameters _camParams;
+
+    tf::TransformBroadcaster _transformBroadcaster;
+    std::list<tf::StampedTransform> _markerTransforms;
+
+
+    void imageCallback(const sensor_msgs::ImageConstPtr &image_msg, const sensor_msgs::CameraInfoConstPtr &camer_info_);
+};
+
+#endif // ARUCO_NODE_H
